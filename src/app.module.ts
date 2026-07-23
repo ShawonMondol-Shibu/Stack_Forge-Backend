27c76b846a7884c8b3d95b81c45d71aa195e313e
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2026-07-24 03:14:11 +0600

    Database: Supabase, ORM: BetterAuth successfully integrated

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { ProfileController } from './profile/profile.controller';
 import { ProfileService } from './profile/profile.service';
 import { ProfileModule } from './profile/profile.module';
-
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './lib/auth/auth';
 @Module({
-  imports: [ProfileModule],
+  imports: [
+    AuthModule.forRoot({
+      auth,
+      bodyParser: {
+        json: { limit: '2mb' },
+        urlencoded: { limit: '2mb', extended: true },
+        rawBody: true,
+      },
+    }),
+    ProfileModule,
+  ],
   controllers: [AppController, ProfileController],
   providers: [AppService, ProfileService],
 })
---
+++ src/lib/auth/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '../database/db'; // your drizzle instance
+import * as schema from '../database/schema';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg', // or "mysql", "sqlite"
+    schema,
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+});
